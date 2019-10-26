d041da48e92542ec9d991b321fad65301dab37ff
Michele61 <54278690+Michele61@users.noreply.github.com>
2019-10-26 19:11:42 +0200

    Add files via upload
    i have fixed "getPWM returns random numbers" issue

--- Adafruit_PWMServoDriver.h
+++ Adafruit_PWMServoDriver.h
@@ -84,7 +84,7 @@ class Adafruit_PWMServoDriver {
   void setExtClk(uint8_t prescale);
   void setPWMFreq(float freq);
   void setOutputMode(bool totempole);
-  uint8_t getPWM(uint8_t num);
+  uint16_t getPWM(uint8_t num);
   void setPWM(uint8_t num, uint16_t on, uint16_t off);
   void setPin(uint8_t num, uint16_t val, bool invert=false);
   uint8_t readPrescale(void);
